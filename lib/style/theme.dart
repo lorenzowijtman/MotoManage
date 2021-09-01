11a6723a00cfae95b4da27ae5380b952cc9a00f8
Lorenzo <lorenzowijtman@hotmail.com>
2021-09-01 19:11:45 +0200

    standard UI changes

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -5,17 +5,20 @@ import 'package:motomanage/style/style.dart';
 abstract class AppTheme {
   // Color template - link to Coolors.co below
   // https://coolors.co/111111-151515-ffffff-d72638
-  static ThemeData themeData = ThemeData(
-      backgroundColor: AppColors.backgroundColor,
-      primaryColor: AppColors.secondaryGrey, // Earie Black
-      accentColor: AppColors.secondaryRed,
-      textTheme: AppTextTheme.theme // Amaranth Red
-      );
+  static ThemeData themeData = ThemeData.dark().copyWith(
+    backgroundColor: AppColors.backgroundColor,
+    primaryColor: AppColors.secondaryGrey, // Earie Black
+    accentColor: AppColors.secondaryRed, // Amaranth Red
+    textTheme: AppTextTheme.theme,
+  );
 }
 
-abstract class AppTextTheme extends TextTheme {
+abstract class AppTextTheme {
   static const TextTheme theme = TextTheme(
-      bodyText1: TextStyle(
-          // Todo
-          ));
+    bodyText1: TextStyle(
+        
+        ),
+  );
 }
+
+abstract class TextStyles {}
